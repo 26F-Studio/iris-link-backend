aa6afd7ae91faed4d732bf0573124376b9346c59
ParticleG <particle_g@outlook.com>
2024-12-15 16:50:30 +0800

    - Init WsControllerTest

--- controllers/api_v1_WsControllerTest.cc
+++ controllers/api_v1_WsControllerTest.cc
@@ -1,18 +1,20 @@
-#include "api_v1_WsControllerTest.h"
+#include <controllers/api_v1_WsControllerTest.h>
 
 using namespace api::v1;
+using namespace std;
 
-void WsControllerTest::handleNewMessage(const WebSocketConnectionPtr& wsConnPtr, std::string &&message, const WebSocketMessageType &type)
-{
+void WsControllerTest::handleNewMessage(
+    const WebSocketConnectionPtr& wsConnPtr,
+    string&& message,
+    const WebSocketMessageType& type
+) {
     // write your application logic here
 }
 
-void WsControllerTest::handleNewConnection(const HttpRequestPtr &req, const WebSocketConnectionPtr& wsConnPtr)
-{
+void WsControllerTest::handleNewConnection(const HttpRequestPtr& req, const WebSocketConnectionPtr& wsConnPtr) {
     // write your application logic here
 }
 
-void WsControllerTest::handleConnectionClosed(const WebSocketConnectionPtr& wsConnPtr)
-{
+void WsControllerTest::handleConnectionClosed(const WebSocketConnectionPtr& wsConnPtr) {
     // write your application logic here
 }
--- controllers/api_v1_WsControllerTest.h
+++ controllers/api_v1_WsControllerTest.h
@@ -4,23 +4,22 @@
 
 using namespace drogon;
 
-namespace api
-{
-namespace v1
-{
-class WsControllerTest : public drogon::WebSocketController<WsControllerTest>
-{
-  public:
-     void handleNewMessage(const WebSocketConnectionPtr&,
-                                  std::string &&,
-                                  const WebSocketMessageType &) override;
-    void handleNewConnection(const HttpRequestPtr &,
-                                     const WebSocketConnectionPtr&) override;
-    void handleConnectionClosed(const WebSocketConnectionPtr&) override;
-    WS_PATH_LIST_BEGIN
-    // list path definitions here;
-    // WS_PATH_ADD("/path", "filter1", "filter2", ...);
-    WS_PATH_LIST_END
-};
-}
+namespace api::v1 {
+    class WsControllerTest : public WebSocketController<WsControllerTest> {
+    public:
+        void handleNewMessage(
+            const WebSocketConnectionPtr&,
+            std::string&&,
+            const WebSocketMessageType&
+        ) override;
+
+        void handleNewConnection(const HttpRequestPtr&, const WebSocketConnectionPtr&) override;
+
+        void handleConnectionClosed(const WebSocketConnectionPtr&) override;
+
+        WS_PATH_LIST_BEGIN
+            // list path definitions here;
+            // WS_PATH_ADD("/path", "filter1", "filter2", ...);
+        WS_PATH_LIST_END
+    };
 }
